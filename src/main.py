ca980ad3c472eaeee09f549f549ef09c88b454d8
Erik Paluka <erik.paluka@gmail.com>
2024-11-22 10:48:25 +0700

    chore(main.py): add missing async features

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import requests
 from telegram import Bot
+from telegram.constants import ParseMode
 from dotenv import load_dotenv
-
+import asyncio
 import os
 
 load_dotenv()
@@ -9,20 +10,18 @@ load_dotenv()
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-print(BOT_TOKEN, CHAT_ID)
-
 
 def get_btc_price():
-    response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+    response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=daddy-tate&vs_currencies=usd")
     response.raise_for_status()
-    price = response.json()['bitcoin']['usd']
+    price = response.json()['daddy-tate']['usd']
     return price
 
-def send_message(price):
+async def send_message(price):
     bot = Bot(token=BOT_TOKEN)
-    message = f"Current BTC Price: ${price:.2f}"
-    bot.send_message(chat_id=CHAT_ID, text=message)
+    message = f"${price:.5f}"
+    await bot.send_message(chat_id=CHAT_ID, text=message, parse_mode=ParseMode.HTML)
 
 if __name__ == "__main__":
     price = get_btc_price()
-    send_message(price)
+    asyncio.run(send_message(price))
