9d5932047d45e068c46a38012426af26061131fd
Erik Paluka <erik.paluka@gmail.com>
2025-10-25 14:32:14 +0700

    add gold and silver scripts

---
+++ src/gold.py
@@ -0,0 +1,38 @@
+import requests
+from telegram import Bot
+from telegram.constants import ParseMode
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+
+def get_btc_price():
+    try:
+        response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=tether-gold&vs_currencies=usd")
+        response.raise_for_status()
+        price = response.json()['daddy-tate']['usd']
+        return price
+    except Exception as e:
+        print(f"Error fetching price: {str(e)}")
+        return None
+
+async def send_message(price):
+    try:
+        if price is not None:
+            bot = Bot(token=BOT_TOKEN)
+            message = f"${price:.5f}"
+            await bot.send_message(chat_id=CHAT_ID, text=message, parse_mode=ParseMode.HTML)
+    except Exception as e:
+        print(f"Error sending message: {str(e)}")
+
+if __name__ == "__main__":
+    try:
+        price = get_btc_price()
+        asyncio.run(send_message(price))
+    except Exception as e:
+        print(f"Main loop error: {str(e)}")
